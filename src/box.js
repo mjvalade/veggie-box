49bd1b8c11bdca797983f14416fe71cf0ce90aff
mjvalade <mjvalade@gmail.com>
2016-08-14 21:39:01 -0600

    render veggies on page

--- src/box.js
+++ src/box.js
@@ -1,14 +1,23 @@
 var $veggieName = $('.veggie-name');
 var $veggieSeason = $('.veggie-season');
 var $submitButton = $('.submit-button');
+var $veggieList = $('.veggie-list');
 
 $submitButton.on('click', function () {
   var options = { name: $veggieName.val(), season: $veggieSeason.val() };
   garden.veggies.push(new Veggie(options));
+  garden.render();
 });
 
 var garden = {
-  veggies: []
+  veggies: [],
+  render: function () {
+    $veggieList.empty();
+    var htmlVeggies = this.veggies.map(function(veggie, index) {
+      return `<li id="veggie-${index}">Name: ${veggie.name}, Season: ${veggie.season}, Pick Date: ${veggie.pickDate || "Not picked yet."}</li>`;
+    });
+    $veggieList.append(htmlVeggies);
+  }
 };
 
 function Veggie (options) {
